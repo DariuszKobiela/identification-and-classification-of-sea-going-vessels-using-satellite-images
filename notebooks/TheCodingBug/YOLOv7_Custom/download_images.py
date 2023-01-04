56556842f27de8e9edfa4a78cbe6a184485e705b
DariuszKobiela <49695347+DariuszKobiela@users.noreply.github.com>
2023-01-04 21:33:46 +0100

    Typo file deleted

--- notebooks/TheCodingBug/YOLOv7_Custom/download_images.py
+++
@@ -1,8 +0,0 @@
-from simple_image_download import simple_image_download as simp
-
-response = simp.simple_image_download
-
-keywords = ["johny depp jack", "jack sparrow"]
-
-for kw in keywords:
-    response().download(kw, 300)
